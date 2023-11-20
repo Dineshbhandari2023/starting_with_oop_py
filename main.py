71d89bc64bab59bb9a5ecf6c8eedf578554c561d
Dinesh Bhandari <133768059+Dineshbhandari2023@users.noreply.github.com>
2023-11-20 23:47:09 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# from tkinter import *
+#
+# root = Tk()
+# root.mainloop()
+# root.title('Registration Form')
+# root.iconbitmap('favicon.ico')
+
+
 class Student:
 
     def __init__(self, dateofbirth, studentname, courseduration, tuitionfee):
@@ -60,16 +68,106 @@ class Regular(Student):
 
     def __init__(self, enrollmentid, dateofbirth, coursename, studentname, dateofenrollement, courseduration, tuitionfee, numofmodules, numofcredithours, dayspresent):
 
+        self.dateofenrollment = None
+        self.enrollmentid = None
         self.numOfModules = numofmodules
         self.numOfCreditHours = numofcredithours
         self.daysPresent = dayspresent
         self.isGrantedScholarship = bool
 
+        super().__init__(dateofbirth, studentname, courseduration, tuitionfee)
+        self.set_coursename(coursename)
+        self.set_enrollmentid(enrollmentid)
+        self.set_dateofenrollment(dateofenrollement)
+
+    def get_numofmodules(self):
+        return self.numOfModules
+
+    def get_numofcreditHours(self):
+        return self.numOfCreditHours
+
+    def get_dayspresent(self):
+        return self.daysPresent
+
+    def get_isgrantedscholarship(self):
+        return self.isGrantedScholarship
+
+    def presentpercentage(self, dayspresent):
+
+        present_percentage = (self.daysPresent / self.courseDuration) * 100
+
+        if 80 <= present_percentage <= 100:
+            is_granted_scholarship = True
+            print("Grade: A")
+        elif 60 <= present_percentage <= 79:
+            is_granted_scholarship = False
+            print("Grade: B")
+        elif 40 <= present_percentage <= 59:
+            is_granted_scholarship = False
+            print("Grade: C")
+        elif 20 <= present_percentage <= 39:
+            is_granted_scholarship = False
+            print("Grade: D")
+        elif 0 <= present_percentage < 20:
+            is_granted_scholarship = False
+            print("Grade: E")
+        else:
+            print("Invalid Number")
+
+    def grantcertificate (self,  coursename, enrollmentid, dateofenrollement ):
+        print(self.get_studentname() + "has been graduated!")
+        print("Course Name: " + self.coursename)
+        print("Enrollment ID: " + self.enrollmentid)
+        print("Date of Enrollment ID: " + self.dateofenrollment)
+
+        if self.isGrantedScholarship:
+            print("The Scholarship has been Granted!!")
+
+    def display(self):
+        super().display()
+        print("Total number of Modules: " + self.numOfModules)
+        print("Total Credit Hours: " + self.numOfCreditHours)
+        print("Total num of days present: " + self.daysPresent)
+
+class Dropout(Student):
+
+    def __init__(self, dateofbirth, studentname, courseduration, tuitionfee, numofremainingmodules,
+                 numofmonthsattended, dateofdropout, coursename, enrollmentid, dateofenrollement):
+
+        self.numOfRemainingModules = numofremainingmodules
+        self.numOfMonthsAttended = numofmonthsattended
+        self.dateOfDropout = dateofdropout
+        self.remainingAmount = 0
+        self.hasPaid = bool
 
         super().__init__(self, dateofbirth, studentname, courseduration, tuitionfee)
         self.set_coursename(coursename)
         self.set_enrollmentid(enrollmentid)
         self.set_dateofenrollment(dateofenrollement)
 
-        
-        
+    def get_numofremainingmodules(self):
+        return self.numOfRemainingModules
+
+    def get_numofmonthsattended(self):
+        return self.numOfMonthsAttended
+
+    def get_dateofdropout(self):
+        return self.dateOfDropout
+
+    def get_remainingamount(self):
+        return self.remainingAmount
+
+    def get_haspaid(self):
+        return self.hasPaid
+
+    def billspayable(self):
+        self.remainingAmount = (self.courseDuration - self.numOfMonthsAttended) * self.tuitionFee
+        self.hasPaid = True
+
+
+
+
+
+
+
+
